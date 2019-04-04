e7c3e5ccaaa6c72f62e29adfe5d78f523e0d2522
Wellington Correia <wellington.correia02@gmail.com>
2019-04-04 04:34:33 -0300

    Inserindo provisóriamente no arquivo, ainda falta ajustar.

--- servidor/server.js
+++ servidor/server.js
@@ -5,12 +5,43 @@ app.listen(3000, () => {
  console.log("Server running on port 3000");
 });
 
-app.post("/create", (req, res, next) => {
- res.json(["Tony","Lisa","Michael","Ginger","Food"]);
+app.get("/getAll", (req, res, next) => {
+
+  var fs = require('fs');
+
+  fs.readFile('db.json', function (err, data) {
+    var json = JSON.parse(data);
+    res.json(json);
+    // fs.writeFile("db.json", JSON.stringify(json));
+  });
 });
 
 app.get("/create", (req, res, next) => {
- res.json(["Tony","Lisa","Michael","Ginger","Food"]);
+
+  var hours = [
+    {
+      start: "10:40",
+      end: "11:00"
+    },
+    {
+      start: "11:40",
+      end: "12:00"
+    }
+  ];
+
+  var registro = {
+    id: 1,
+    day: "03-04-2019",
+    intervals: hours
+  }
+  //var json = JSON.parse(registro);
+  res.json(registro);
+
+  var fs = require('fs');
+  fs.readFile('db.json', function (err, data) {
+    fs.writeFile("db.json", JSON.stringify(registro))
+  });
+
 });
 
 app.delete("/delete/:regraId", (req, res, next) => {
