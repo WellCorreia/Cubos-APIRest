cf54e41769a152aa89c0b27aab431bcc5531efae
Wellington Santos <wellington.santos@cto.local>
2019-04-05 16:11:38 -0300

    Inserido nova funcionalidade de inserir por quantidade de dias e por semana

--- app/controllers/atendimento.controller.ts
+++ app/controllers/atendimento.controller.ts
@@ -14,7 +14,99 @@ exports.create = function(req, res) {
 				}
 				res.json(json);
 			}
-    	fs.writeFile("db.json", JSON.stringify(json, null, 4));
+    	fs.writeFile("db.json", JSON.stringify(json, null, 4),{}, () => {
+         fs.close(5, () => {});
+      });
+		});
+};
+
+exports.createWeek = function(req, res) {
+    var newAtendimento = req.body;
+		var json = [];
+    var datafim;
+    var dateString = newAtendimento.day;
+    var count = 0;
+    var fs = require('fs');
+
+    var date1 = new Date(dateString);
+    var date = (date1.getFullYear()+'-'+date1.getDate()+'-0'+date1.getMonth());//Date((date1.getMonth()+1)+'-'+date1.getDate()+'-'+date1.getFullYear());
+    date = new Date(date);
+
+		fs.readFile('db.json', function (err, data, bytes) {
+
+      for(let i = 0; i < 7; i++){
+        date.setDate(date.getDate() + 1);
+        if(date.getDate() < 10){
+            datafim = (('0'+date.getDate())+'-'+(date.getMonth()+1)+'-'+date.getFullYear());
+        }else{
+            datafim = ((date.getDate())+'-'+(date.getMonth()+1)+'-'+date.getFullYear());
+        }
+        newAtendimento.day = datafim;
+        console.log(datafim)
+        if(json != "" && json != null){
+          if(data != "" ){
+            if(count == 0){
+              json = JSON.parse(data);
+              count++;
+            }
+  				}
+          json.atendimentos.push({...newAtendimento});
+  			}else{
+  				json = {
+  					atendimentos:[{...newAtendimento}]
+  				}
+  			}
+      }
+      res.json(json);
+      fs.writeFile("db.json", JSON.stringify(json, null, 4),{}, () => {
+        fs.close(5, () => {});
+      });
+		});
+};
+
+exports.createDay = function(req, res) {
+    var newAtendimento = req.body;
+		var json = [];
+    var datafim;
+    var dateString = newAtendimento.day;
+    var count = 0;
+    var fs = require('fs');
+
+    var date1 = new Date(dateString);
+    var date = (date1.getFullYear()+'-'+date1.getDate()+'-0'+date1.getMonth());//Date((date1.getMonth()+1)+'-'+date1.getDate()+'-'+date1.getFullYear());
+    date = new Date(date);
+
+		fs.readFile('db.json', function (err, data, bytes) {
+      for(let i = 0; i < req.params.qtd; i++){
+        date.setDate(date.getDate() + 1);
+        if(date.getDate() < 10){
+            datafim = (('0'+date.getDate())+'-'+(date.getMonth()+1)+'-'+date.getFullYear());
+        }else{
+            datafim = ((date.getDate())+'-'+(date.getMonth()+1)+'-'+date.getFullYear());
+        }
+
+        newAtendimento.day = datafim;
+
+        if(json != "" && json != null){
+          if(data != "" ){
+            if(count == 0){
+              json = JSON.parse(data);
+              count++;
+            }
+  				}
+          console.log(newAtendimento)
+          json.atendimentos.push({...newAtendimento});
+  			}else{
+          json = {
+  					atendimentos:[{...newAtendimento}]
+  				}
+  			}
+      }
+      res.json(json);
+      //console.log(json)
+      fs.writeFile("db.json", JSON.stringify(json, null, 4),{}, () => {
+        fs.close(5, () => {});
+      });
 		});
 };
 
--- app/routes/atendimento.routes.ts
+++ app/routes/atendimento.routes.ts
@@ -5,6 +5,12 @@ module.exports = function(app) {
     // Cria um novo Atendimento
     app.post('/api/atendimentos', atendimentos.create);
 
+    // Cria um novo Atendimento Semanal
+    app.post('/api/atendimentos/semanal', atendimentos.createWeek);
+
+    // Cria um novo Atendimento Diario
+    app.post('/api/atendimentos/diario/:qtd', atendimentos.createDay);
+
     // Retorna todos os Atendimento
     app.get('/api/atendimentos', atendimentos.findAll);
 
